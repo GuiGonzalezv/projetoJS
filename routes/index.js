117e1d3d2700f8f7f773b9e6106243a78b81b6b2
Guilherme <gonzalez.etec@hotmail.com>
2018-09-28 16:34:27 +0000

    update

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var moment = require('moment');
+var nosql = require("../lib/nosql.js")
+var moment = require('moment-timezone');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,7 +9,39 @@ router.get('/', function(req, res, next) {
 });
 
 router.all("/save", function(req, res){
-  var dados = req.query
+  
+	var dados = req.query
+	dados.link = "linkhttp: 2010231"
+
+	var dataExpiracao = new moment()
+	var err = {}
+	if(dados){
+		
+		if(!dados.titulo){
+			err.titulo = "Insira um titulo correto!"
+		}
+		if(!dados.texto){
+			err.texto = "Insira o texto!"
+		}
+		if(Object.keys(err).length == 0){
+			console.log("salvo")
+			res.json({
+				status: 200,
+				dados: dados
+			})
+		}else{
+			res.json({
+				status: 404,
+				dados: err
+			})
+		}
+
+	}else{
+		err.geral = "Insira os dados corretamente!"
+	}
+
+	dataExpiracao = dataExpiracao.toDate()
+	console.log(dataExpiracao)
 
   
 })
