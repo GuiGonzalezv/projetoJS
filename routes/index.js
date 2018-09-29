cbae36b1f27eabf3906c214c76adb57b2c0a41b3
Guilherme <gonzalez.etec@hotmail.com>
2018-09-29 03:47:21 +0000

    salvando links

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var nosql = require("../lib/nosql.js")
 var moment = require('moment-timezone');
+var md5 = require('md5');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,24 +13,67 @@ router.get('/', function(req, res, next) {
 router.all("/save", function(req, res){
   
 	var dados = req.query
-	dados.link = "linkhttp: 2010231"
+
+	dados.link = md5(dados.titulo)
+
+	dados.criadoEm = new moment().toDate()
 
 	var dataExpiracao = new moment()
 	var err = {}
 	if(dados){
 		
+		if(dados.expira == "5 min"){
+			dataExpiracao.add(5, 'minutes')
+			dataExpiracao = dataExpiracao.toDate()		
+		}
+		else if(dados.expira == "10 min"){
+			dataExpiracao.add(10, 'minutes')
+			dataExpiracao = dataExpiracao.toDate()	
+		}
+		else if(dados.expira == "1 Semana"){
+			dataExpiracao.add(1, 'week')
+			dataExpiracao = dataExpiracao.toDate()	
+		}
+		else if(dados.expira == "1 Mes"){
+			dataExpiracao.add(1, 'month')
+			dataExpiracao = dataExpiracao.toDate()	
+		}
+		else if(dados.expira == "1 Ano"){
+			dataExpiracao.add(1, 'year')
+			dataExpiracao = dataExpiracao.toDate()	
+		}else{
+			dataExpiracao = false
+		}
+		dados.expira = dataExpiracao
+
 		if(!dados.titulo){
 			err.titulo = "Insira um titulo correto!"
 		}
-		if(!dados.texto){
+		else if(!dados.texto){
 			err.texto = "Insira o texto!"
 		}
 		if(Object.keys(err).length == 0){
-			console.log("salvo")
-			res.json({
-				status: 200,
-				dados: dados
+			nosql.getDB(function(db){
+
+				db.collection("pasteBin").insertOne(dados, function(err, result){
+					if(err){
+						err.geral = "Erro ao inserir os dados"
+						res.json({
+							status: 404,
+							dados: err
+						})
+					}else{
+						res.json({
+							status: 200,
+							dados: dados
+						})
+					}
+				})
+
+				
 			})
+			// console.log("salvo")
+			
 		}else{
 			res.json({
 				status: 404,
@@ -40,8 +85,6 @@ router.all("/save", function(req, res){
 		err.geral = "Insira os dados corretamente!"
 	}
 
-	dataExpiracao = dataExpiracao.toDate()
-	console.log(dataExpiracao)
 
   
 })
